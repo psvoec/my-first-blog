9a569237a830e631bd25ca23560e7dedf98ba067
Vamsi <pediredla.saivamsi@gmail.com>
2026-03-23 06:57:57 +0530

    modified new code

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'myapp/post_list.html', {})
+    posts = post.objects.filter(published_date_lte=time-zone.now()).order_by('published_date')
+    return render(request, 'myapp/post_list.html', {'posts':posts})
